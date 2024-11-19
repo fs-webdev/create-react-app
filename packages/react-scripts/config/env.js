9135cd224951f07526de4bdef90968d10dc3f066
Joey Cozza <joeycozza@gmail.com>
2024-11-19 09:49:54 -0700

    undo dep updates

--- packages/react-scripts/config/env.js
+++ packages/react-scripts/config/env.js
@@ -40,7 +40,7 @@ const dotenvFiles = [
 // https://github.com/motdotla/dotenv-expand
 dotenvFiles.forEach(dotenvFile => {
   if (fs.existsSync(dotenvFile)) {
-    require('dotenv-expand').expand(
+    require('dotenv-expand')(
       require('dotenv').config({
         path: dotenvFile,
       })
